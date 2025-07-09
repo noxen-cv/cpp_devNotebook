da23d8a8b518821f6d55003a98e4cc1afd896300
noxen-cv <christian.valenzuela.4339@gmail.com>
2025-07-09 19:14:29 +0800

    added math v2.cpp

--- gfg-Trials/Loops/math.cpp
+++ gfg-Trials/Loops/math.cpp
@@ -4,10 +4,45 @@
 
 using namespace std;
 
+class preMath {
+public:
+    void countDigits(int n) {
+        int count = 0;
+    
+        while (n > 0) {
+            n /= 10;
+            count++;
+        }
+    
+        cout << count;
+    }
+};
+
 class math {
     public: 
+
+    void printDivisors(int n) {
+        // code here
+        for(int i = 1; i <= n; i++){ 
+            if (n % i == 0) {
+                cout << i << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    void factorial(int n) {
+        // code here
+        int ans = 1;
+        
+            while (n > 0) {
+                ans *= n;
+                n--;
+            }
+        cout << ans << endl;
+    }
     
-    void greatestCommonDivisor(int a, int b) {
+    void gcd(int a, int b) {
     int x = min(a,b);
     int ans = 1;
     
@@ -18,5 +53,26 @@ class math {
         }
     cout << ans << endl;
     }
+
+    int gcdEuclid(int a, int b) {
+        while (b != 0) {
+            int temp = b; 
+            b = a % b;
+            a = temp;
+        }
+    return a;
+    }
+
+    void lcd(int a, int b) {
+        if (a == 0 || b == 0) {
+            cout << 0 << endl;
+        }
+        cout << (a * b) / gcdEuclid(a,b) << endl;
+    }
    
 };
+
+int main() {
+
+    return 0;
+}
