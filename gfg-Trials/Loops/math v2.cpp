c5988f4eababb899caca4ae1cec7ebda8ee31906
noxen-cv <christian.valenzuela.4339@gmail.com>
2025-07-12 23:30:32 +0800

    final ver of mathv2.cpp - done

--- gfg-Trials/Loops/math v2.cpp
+++ gfg-Trials/Loops/math v2.cpp
@@ -36,9 +36,44 @@ class mathv2 {
             }
         }
 
+        void primeNum(int n) {
+                if(n == 0 || n == 1){
+                    cout << "Neither Prime nor Composite";
+                }
+                
+                int i = 2;
+                while(i < n){
+                    if(n % i == 0){
+                        cout << "No";
+                    }
+                    i++;
+                }
+                cout << "Yes";
+        }
+
+        void multable(int n) {
+            int m = 10;
+            for (int i = 1; i<=m; i++){
+                cout << n * i << "\n";
+            }
+        }
+
+        void tableDiffernce(int n, int m){
+             int limit = 10;
+                for (int i = 1; i <= limit; i++){
+                    cout << (n * i - m * i);
+                        if (i != limit){
+                                cout << " ";
+                            }
+                    }
+            }
+
 };
 
 int main () {
     int n;
 
+
+
+    
 }
