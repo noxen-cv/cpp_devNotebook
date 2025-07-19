5eaa827d5a09967776c3f9417b6c612fb47ab295
noxen-cv <christian.valenzuela.4339@gmail.com>
2025-07-20 00:01:19 +0800

    New function arguments

---
+++ gfg-Trials/Functions/functions.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+
+// Functions in C++
+
+ // Function with no arguments;
+ void greet() {
+    std::cout << "Hello, World!" << std::endl;
+ }
+
+ // Function with argument parameters
+ void greetUser(std::string name) {
+    std::cout << "Hello, " << name << "!" << std::endl; 
+
+ }
+
+ // Function with return type
+int returnValue(int a, int b) {
+
+}
+
+
+// Default Arguments
+
+
+int main() {
+
+
+ }
