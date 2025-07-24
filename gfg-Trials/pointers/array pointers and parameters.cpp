0a0920a0d474758044fc0d9c842c200166408197
noxen-cv <christian.valenzuela.4339@gmail.com>
2025-07-24 16:49:00 +0800

    array pointer parameters.cpp

---
+++ gfg-Trials/pointers/array pointers and parameters.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void normalLoops(int* n, int size) {
+    for(int i = 0; i < size; ++i) {
+        cout << n[i] << " ";
+    }
+}
+
+
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    // rangebased for loop
+    for (int x: arr) {
+        cout << x << " "; // Output each element in the array
+    }
+
+    return 0;
+}
