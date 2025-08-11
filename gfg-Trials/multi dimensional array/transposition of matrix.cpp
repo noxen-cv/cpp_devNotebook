f2913c0019c410d537270a32670a0c0dd5895295
noxen-cv <christian.valenzuela.4339@gmail.com>
2025-08-11 12:17:01 +0800

    added new func in transpose of matrix

--- gfg-Trials/multi dimensional array/transposition of matrix.cpp
+++ gfg-Trials/multi dimensional array/transposition of matrix.cpp
@@ -15,13 +15,13 @@ class transpose {
 
     public:
     // Constructor Getters and Setters method
-    transpose(const vector<vector<int>>& vec) : vec1(vec) {
+    transpose(const vector<vector<int>>& vec, int& size) : vec1(vec), size(size) {
         
     }    
 
-    void setValue(int val) { 
-        for(int i = 0; i < val; i++) {
-            for(int j = 0; j < val; j++) {
+    void setValue() { 
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++) {
                 int val;
                 cin >> val;
                 vec1[i][j] = val;
@@ -29,8 +29,24 @@ class transpose {
         }
     }
 
+    void printVec() {
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++) {
+                cout << vec1[i][j] << " ";
+            }
+        cout << endl;
+        }   
+    }
+
     void swap() {
-    
+        ans.resize(size, vector<int>(size, 0)); // resize() operato for declare first, fill later
+
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < size; i++) {
+                ans[i][j] = vec1[i][j];
+            }
+            cout << endl;
+        }
     }
 
 };
@@ -43,12 +59,12 @@ int main() {
     int size;       // Maintaining Square 2d array with same rows and cols
     int val;        // Value Index inside the Matrix;
 
-    vector<vector<int>> vec(size, vector<int>(size, 0));       
+    vector<vector<int>> vec(size, vector<int>(size, 0));  // Syntax Breakdown: vector<vector<int>> name(rows, vector<int>(cols, init_value)); declare and initialized
 
     print("Set the Fixed Square Size: ");
     cin >> size;
-    transpose tr(vec);
-    tr.setValue(size);
+    transpose tr(vec, size);
+    tr.setValue();
 
 
 
