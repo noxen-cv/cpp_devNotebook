037ccb337b35d278c70176411675a5c837898011
noxen-cv <christian.valenzuela.4339@gmail.com>
2025-08-12 06:20:16 +0800

    added transpose and swap func

--- gfg-Trials/multi dimensional array/transpose of a matrix v2.cpp
+++ gfg-Trials/multi dimensional array/transpose of a matrix v2.cpp
@@ -5,17 +5,34 @@
 
 using namespace std;
 
-void transpose(int arr[4][4]) {
+void transpose(int arr[4][4]) {             // Fixed size parameter 4x4
     for(int i = 0; i < 4; i++) {
         for(int j = i+1; j < 4; j++) {
-            int temp = arr[i][j];
-            arr[i][j] = arr[j][i];
+            int temp = arr[i][j];           // using the manual swapping
+            arr[i][j] = arr[j][i];          // or do the swap() from <algorithm>
             arr[j][i] = temp;
+        }
+    }
+}
+
+void swap(int mat[4][4]) {
+        for(int i = 0; i < 4; i++) {
+        for(int j = i+1; j < 4; j++) {
+            swap(mat[i][j],mat[j][i]);      // using swap() from algorithm
         }
     }
 }
 
 int main(){
 
+    int mat[4][4] = {
+        {1, 2, 3, 4},
+        {1, 2, 3, 4},
+        {1, 2, 3, 4},
+        {1, 2, 3, 4}
+    };
 
+    transpose(mat);
+
+    return 0;
 }
