f7ce7b4a01d1effc29a8a1cbbbb7f4d37879f1fb
noxen-cv <christian.valenzuela.4339@gmail.com>
2025-08-20 15:13:03 +0800

    added comments

--- gfg-Trials/templates/maximum func template.cpp
+++ gfg-Trials/templates/maximum func template.cpp
@@ -9,5 +9,17 @@ T myMax(T x, T y) {
 int main() {
     cout << myMax<int>(3, 7) << endl;      // Max of two integers
     cout << myMax<char>('c', 'g') << endl; // Max of two characters
-    return 0;
-}
+ 
+    
+
+
+/* NEEDS FOR AREAS FOR IPROVEMENTS
+
+USING TEMPLATE 
+
+
+
+
+
+
+*/
