4ae142faee2967a46aee6200662fe24209a6828e
noxen-cv <christian.valenzuela.4339@gmail.com>
2025-08-26 06:02:46 +0800

    added generic sort

---
+++ gfg-Trials/templates/Generic Sort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+template <class T>
+void sortArray(T arr[], int n) {
+    sort(arr, arr + n);             // Syntax of sort: sort(first, last); and it automatically
+}                                   // sorts either alphabetically, or lexicographically
+
+template <typename T>       // Using <typename T> and <class T> is the same
+void printArray(T arr[], int n) {
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+}
+
+template <typename T>
+void selection(T arr[], int n) {
+    T arr[];
+    
+}
+
+
+int main() {
+    int n;
+    float x;
+
+
+    sortArray()
+
+}
