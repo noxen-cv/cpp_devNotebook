2d8589c69e2aec0e8f88d55176a86691d828ba17
noxen-cv <christian.valenzuela.4339@gmail.com>
2025-11-12 07:50:25 +0800

    added new file in oop inheritance

---
+++ src/gfg-Trials/Object-Oriented Programming/static/inheritannce/multi-level-Inheritance.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+using namespace std;
+
+class Student {
+    protected:
+        int roll_number;
+    public:
+        void set_rollnumber(int no) {
+                no = roll_number;
+        }
+
+        virtual ~Student() {}
+};
+
+class Test : public Student {
+    protected:
+        int s1, s2, s3, s4, s5;
+    public:
+        void set_Marks(const float* Marks_array) {
+            s1 = Marks_array[0];
+            s2 = Marks_array[1];
+            s3 = Marks_array[2];
+            s4 = Marks_array[3];
+            s5 = Marks_array[4];
+        }    
+    virtual ~Test() {}
+};
+
+class Result : public Test {
+        void Display() {
+            int total_Marks = s1 + s2 + s3 + s4 +5;
+
+            int avg_Marks = total_Marks / 5;
+
+            cout << "Roll Number: " << roll_number << endl;
+            cout << "Total Marks: " << total_Marks << endl;
+            cout << "Average: " << avg_Marks << endl;
+        }
+};
+
+int main(){
+    Result studentData;
+    int studentCount;
+    int rollNum;
+    float inputMarks[5];
+    
+    
+    // User Input
+    cout << "Enter Number of Students: ";
+    cin >> studentCount;
+
+    for(int i = 1; i < studentCount; i++) {
+        rollNum = i;
+
+        cout << "Entering Data for Roll num: " << rollNum << endl;
+
+        for(int j = 0; j < 5; ++j) {
+            cout << "Enter Mark for Subject: ";
+            cin >> inputMarks[j];
+            j++;
+        }
+    }
+
+    
+
+
+
+}
